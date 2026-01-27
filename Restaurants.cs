e2d0526c6c87ed46ad9ba605fb782acd43ab0ef0
estherteo5521 <estherteo925@gmail.com>
2026-01-27 15:24:06 +0800

    commentupdated

--- Restaurants.cs
+++ Restaurants.cs
@@ -13,7 +13,7 @@ namespace Gruberooapp
         public string RestaurantName { get; set; }
         public string RestaurantEmail { get; set; }
 
-        public List<FoodItem> Menu { get; set; } = new List<FoodItem>(); // Use FoodItem directly
+        public List<FoodItem> Menu { get; set; } = new List<FoodItem>(); 
         private List<SpecialOffer> specialOffers = new List<SpecialOffer>();
         private List<Order> orders = new List<Order>();
         public Queue<Order> OrderQueue { get; set; } = new Queue<Order>();
@@ -27,33 +27,33 @@ namespace Gruberooapp
 
         public void AddOrder(Order order)
         {
-            orders.Add(order);            // add to internal list
-            OrderQueue.Enqueue(order);    // add to queue
+            orders.Add(order);            
+            OrderQueue.Enqueue(order);    
         }
 
 
-        // Add food item to menu
+        
         public void AddMenuItem(FoodItem item)
         {
             Menu.Add(item);
         }
 
-        // Remove food item
+      
         public bool RemoveMenuItem(FoodItem item)
         {
             return Menu.Remove(item);
         }
 
-        // Display menu
+   
         public void DisplayMenu()
         {
             foreach (var item in Menu)
             {
-                Console.WriteLine(item); // make sure FoodItem has ToString()
+                Console.WriteLine(item); 
             }
         }
 
-        // Display special offers
+        
         public void DisplaySpecialOffers()
         {
             foreach (var offer in specialOffers)
@@ -62,7 +62,7 @@ namespace Gruberooapp
             }
         }
 
-        // Display orders
+        
         public void DisplayOrders()
         {
             foreach (var order in orders)
