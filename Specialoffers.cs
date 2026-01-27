29092ee5839d5db3901f020d5ee5225cbe5be4ef
estherteo5521 <estherteo925@gmail.com>
2026-01-27 14:30:32 +0800

    Specialoffers.cs updated

---
+++ Specialoffers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruberooapp
+{
+    public class SpecialOffer
+    {
+        public string OfferCode { get; set; }
+        public string OfferDesc { get; set; }
+        public double Discount { get; set; }
+
+        public SpecialOffer(string code, string desc, double discount)
+        {
+            OfferCode = code;
+            OfferDesc = desc;
+            Discount = discount;
+        }
+
+        public override string ToString()
+        {
+            return $"{OfferCode}: {OfferDesc} ({Discount * 100}% off)";
+        }
+    }
+
+
+}
