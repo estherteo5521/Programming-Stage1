c49d9379380efe2021809e48919923e4fe54285d
estherteo5521 <estherteo925@gmail.com>
2026-01-27 13:16:57 +0800

    Menu and order Cs

---
+++ orderedfooditems.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruberooapp
+{
+    public class OrderItem
+    {
+        public FoodItem Item { get; set; }
+        public int Quantity { get; set; }
+
+        public OrderItem(FoodItem item, int quantity)
+        {
+            if (item == null) throw new ArgumentNullException(nameof(item));
+            Item = item;
+            Quantity = quantity;
+        }
+
+        // Safe Subtotal calculation
+        public double Subtotal()
+        {
+            return Item.Price * Quantity; // guaranteed to exist
+        }
+
+        public override string ToString()
+        {
+            return $"{Item.Name} x {Quantity} = ${Subtotal():F2}";
+        }
+    }
+
+
+
+
+}
+
+
+
+
+
+
