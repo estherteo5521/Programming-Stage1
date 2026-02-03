8ffcc4819f6edf95d25e4adb6aa2b5d13ca25d51
Your Name <yapjiaxuan050712@gamil.com>
2026-02-03 13:23:54 +0800

    add new file

---
+++ FoodItem.cs
@@ -0,0 +1,32 @@
+﻿//========================================================== 
+// Student Number : S10272963E
+// Student Name : Yap Jia Xuan 
+// Partner Name : Esther Teo Hui Min
+//==========================================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10272963_PRG2ASG
+{
+    abstract class FoodItem
+    {
+        public string itemName {  get; set; }
+        public string itemDesc { get; set; }
+        public string itemPrice { get; set; }
+        public string customise { get; set; }
+        public FoodItem (string itemname, string itemdesc, string itemprice,string Customise)
+        {
+            itemName = itemname;
+            itemDesc = itemdesc;
+            itemPrice = itemprice;
+            customise = Customise;
+        }
+        public override string ToString()
+        {
+            return "Food item" + itemName + itemDesc + itemPrice + customise;
+        }
+    }
+}
---
+++ OrderFoodItem.cs
@@ -0,0 +1,33 @@
+﻿//========================================================== 
+// Student Number : S10272963E
+// Student Name : Yap Jia Xuan 
+// Partner Name : Esther Teo Hui Min
+//==========================================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10272963_PRG2ASG
+{
+    internal class OrderFoodItem:FoodItem
+    {
+        public int qtyOrdered {  get; set; }
+        public double subTotal { get; set; }
+        public OrderFoodItem(string itemname, string itemdesc, string itemprice, string Customise, int quantityorder, double subtotal) : base( itemname,itemdesc,itemprice,Customise)
+        {
+            qtyOrdered = quantityorder;
+            subTotal = subtotal;
+        }
+
+        public double CalculateSubtotal ()
+        {
+            //subTotal = qtyOrdered * itemPrice;
+            return subTotal;
+        }
+
+       
+    }
+}
+
